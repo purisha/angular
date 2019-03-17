4672c8064726f0d8504c67edaf24e4e2b8a80b40
purisha <32126374+purisha@users.noreply.github.com>
2019-03-17 20:00:13 +0330

    Add files via upload

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit} from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators  } from '@angular/forms';
+import { User } from '../User';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+ 
+   loginForm: FormGroup;
+    
+   constructor(private fb:FormBuilder) {}
+   
+    get email() { return this.loginForm.get('email'); }
+     
+    get password() { return this.loginForm.get('password'); }
+ 
+    get terms() { return this.loginForm.get('terms'); }
+	
+    ngOnInit() {
+
+        this.loginForm  = this.fb.group({
+            email: ['',[Validators.required,
+                        Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+            password: this.fb.group({
+                pwd: ['', [Validators.required, 
+                           Validators.minLength(8)]],
+                confirmPwd: ['', [Validators.required,
+                                  Validators.minLength(8)]]
+            }),
+	 })
+	
+		 
+  }
+}
+
