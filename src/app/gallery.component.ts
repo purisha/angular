3b4310752037beeebf33171342768123129b2400
purisha <32126374+purisha@users.noreply.github.com>
2019-03-17 18:32:02 +0330

    Add files via upload

---
+++ src/app/gallery.component.ts
@@ -0,0 +1,21 @@
+import { Component,  OnInit } from '@angular/core';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
+
+
+@Component({
+  selector: 'app-gallery',
+  templateUrl: './gallery.component.html',
+  styleUrls: ['./gallery.component.css']
+})
+
+export class GalleryComponent implements OnInit {
+
+
+ ngOnInit() {
+    }
+}
+ 
+
+
+ 
+
