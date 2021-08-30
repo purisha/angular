239c39d670861328456c5c2c2b9142a8fec016de
purisha <32126374+purisha@users.noreply.github.com>
2021-08-30 19:22:57 +0430

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  itemImageUrl='./../assets/images/gallery/rome7.jpg';
   constructor() { }
   ngOnInit() {
   }
