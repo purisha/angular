169542d092c97d0fe6b09bf8a1dea1563723c051
purisha <32126374+purisha@users.noreply.github.com>
2025-12-30 00:30:36 -0800

    Update user.ts

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,18 +1,23 @@
 export class User {
- 
-    id: number;
-    email: string;
-    //Both the passwords are in a single object
-    password: { 
-      pwd: string;
-      confirmPwd: string;
-    };
-     
-    terms: boolean;
- 
-    constructor(values: Object = {}) {
-      //Constructor initialization
-      Object.assign(this, values);
+
+  id: number;
+  email: string;
+  //Both the passwords are in a single object
+  password: {
+    pwd: string;
+    confirmPwd: string;
+  };
+
+  terms: boolean;
+
+
+
+  constructor(values: any = {}) {
+    //Constructor initialization
+    this.id = values.id || null;
+    this.email = values.email || '';
+    this.password = values.password || { pwd: '', confirmPwd: '' };
+    this.terms = values.terms || false;
+
   }
- 
 }
